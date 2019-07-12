62d5e6ff265cb40722cfded3fc7dccccf90b5148
dim912 <dim912@gmail.com>
2019-07-12 23:29:38 +0800

    renamed body content to landing page body content

---
+++ src/client/components/bodyContent/LaondingPageBodyContent.js
@@ -0,0 +1,61 @@
+
+import React from 'react'
+import { Input } from 'antd';
+import style from './LaondingPageBodyContent.less'
+import SearchBox from './searchBox/SearchBox'
+
+
+
+const Search = Input.Search
+
+class LaondingPageBodyContent extends React.Component {
+
+    constructor( props ) {
+        super( props )
+        this.state = { showLogin: true }
+    }
+
+    render() {
+
+        return <React.Fragment>
+
+
+            <div className={ style.leftNavBar }> </div>
+            <div className={ style.middleContent }>
+
+                <div className={ style.moto }>
+                    Find Your Dream Job with Gorilla
+                </div>
+
+
+                <div className={ style.SearchArea }>
+
+                    <div className={ style.searchBox }>
+                        <SearchBox />
+                    </div>
+
+                    {/*
+                    <div className={ style.registerNowBox }>
+                        <RegisterNowBox />
+                    </div>
+                    */}
+
+
+
+
+                </div>
+
+                <div>
+                </div>
+
+            </div>
+            <div className={ style.rightNavBar }> </div>
+
+        </React.Fragment >
+    }
+}
+
+
+
+export default BodyContent
+
