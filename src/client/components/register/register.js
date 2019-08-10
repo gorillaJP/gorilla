9b50ea4a22df05b91aec6fce2cfcddaa38738cca
dim912 <dim912@gmail.com>
2019-08-10 13:10:07 +0800

    added register

--- src/client/components/register/register.js
+++ src/client/components/register/register.js
@@ -1,122 +1,141 @@
-
 import React from 'react'
 import { Form, Icon, Input, Button, Modal, Checkbox } from 'antd';
 import style from './register.less'
 
+const Lable = ( props ) => {
 
+    return <div style={ { display: 'inline-block', width: '100px' } }>
+        <div style={ { display: 'inline', color: 'red' } } >
+            { props.required ? '* ' : '' }
+        </div>
+        <div style={ { display: 'inline' } }>
+            { props.text }
+        </div>
+    </div >
 
+}
+
+const firstNameLabel = <Lable text="First Name" required={ true } />
+const surName = <Lable text="Surname" required={ true } />
+const email = <Lable text="Email" required={ true } />
+const password = <Lable text="Password" required={ true } />
 
 class Register extends React.Component {
 
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFields( ( err, values ) => {
+
+            if ( err )
+                return
+
+
+            
+        } );
+    };
 
     render() {
 
-        const formItemLayout = {
-            labelCol: {
-                xs: { span: 24 },
-                sm: { span: 8 },
-            },
-            wrapperCol: {
-                xs: { span: 24 },
-                sm: { span: 16 },
-            },
-        };
-
         const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
 
-
-        return <React.Fragment { ...formItemLayout }>
+        return <React.Fragment>
 
             <div className={ style.bodyContainer }>
 
+                <div className={ style.formContainer }>
+
+                    <Form hideRequiredMark={ true } onSubmit={ this.handleSubmit } className={ style.form }>
+                        < div className={ style.leftPanel } >
+                            <Form.Item label={ firstNameLabel } className={ style.testStyle }>
+                                { getFieldDecorator( 'firstName', {
+                                    validate: [ {
+                                        trigger: 'onBlur',
+                                        rules: [
+                                            {
+                                                required: true,
+                                                message: <div style={ { display: 'inline' } }></div>
+                                            }
+                                        ]
+                                    } ]
+
+                                } )(
+                                    <Input />,
+                                ) }
+
+                            </Form.Item>
+
+                            <Form.Item label={ surName } className={ style.testStyle } >
+                                { getFieldDecorator( 'surname', {
+                                    validate: [ {
+                                        trigger: 'onBlur',
+                                        rules: [
+                                            {
+                                                required: true,
+                                                message: <div></div>
+                                            }
+                                        ]
+                                    } ]
+
+                                } )(
+                                    <Input />,
+                                ) }
+
+                            </Form.Item>
 
 
-                <div className={ style.leftPanel }>
+                            <Form.Item label={ email } className={ style.testStyle } >
+                                { getFieldDecorator( 'email', {
+                                    validate: [ {
+                                        trigger: 'onBlur',
+                                        rules: [
+                                            {
+                                                required: true,
+                                                message: <div></div>,
+                                            }
+                                        ]
+                                    } ]
 
-                    <Form.Item label="First Name" >
-                        { getFieldDecorator( 'firstName', {
-                            validate: [ {
-                                trigger: 'onBlur',
-                                rules: [
-                                    {
-                                        required: true,
-                                        message: 'Please enter a username',
-                                    }
-                                ]
-                            } ]
+                                } )(
+                                    <Input />,
+                                ) }
 
-                        } )(
-                            <Input
-                            />,
-                        ) }
+                            </Form.Item>
 
-                    </Form.Item>
+                            <Form.Item label={ password } className={ style.testStyle } >
+                                { getFieldDecorator( 'password', {
+                                    validate: [ {
+                                        trigger: 'onBlur',
+                                        rules: [
+                                            {
+                                                required: true,
+                                                message: <div></div>
+                                            }
+                                        ]
+                                    } ]
 
-                    <Form.Item label="Surname" >
-                        { getFieldDecorator( 'surName', {
-                            validate: [ {
-                                trigger: 'onBlur',
-                                rules: [
-                                    {
-                                        required: true,
-                                        message: 'Please enter a username',
-                                    }
-                                ]
-                            } ]
+                                } )(
+                                    <Input
+                                        type="password"
+                                    />,
+                                ) }
 
-                        } )(
-                            <Input
-                                placeholder="Sur Name"
-                            />,
-                        ) }
+                            </Form.Item>
 
-                    </Form.Item>
-
+                            <Button style={ {
+                                size: 'large ',
+                                width: '100%', marginBottom: '10px',
+                                marginTop: '20px'
+                            } } type="primary" htmlType="submit" className="login-form-button">
+                                Register
+                            </Button>
+                        </div>
+                    </Form>
                 </div>
-                <div className={ style.rightPanel }>
-
-                    <Form.Item label="First Name" >
-                        { getFieldDecorator( 'firstName', {
-                            validate: [ {
-                                trigger: 'onBlur',
-                                rules: [
-                                    {
-                                        required: true,
-                                        message: 'Please enter a username',
-                                    }
-                                ]
-                            } ]
-
-                        } )(
-                            <Input
-                            />,
-                        ) }
-
-                    </Form.Item>
-
-
-
-
-
-                </div>
-
-
             </div>
-
-
-
         </React.Fragment >
     }
-
 }
 
 const RegisterForm = Form.create( { name: 'register' } )( Register );
 
 export default RegisterForm
-
-
-
-
-
-
-
