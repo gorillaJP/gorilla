ce60839c1f07204416bdd12fb23dd3db5ccd3dc9
dim912 <dim912@gmail.com>
2019-11-29 23:14:48 +0800

    test change3

--- src/client/components/bodyContent/LaondingPageBodyContent.js
+++ src/client/components/bodyContent/LaondingPageBodyContent.js
@@ -24,7 +24,7 @@ class LaondingPageBodyContent extends React.Component {
             <div className={ style.middleContent }>
 
                 <div className={ style.moto }>
-                    Find Your Dream Job with Gorilla..!y
+                    Find Your Dream Job with Gorilla..!z
                 </div>
 
 
