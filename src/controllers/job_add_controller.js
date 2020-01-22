adb42e8dbf91e9ba33935db00c4468347e08efb0
dim912 <dim912@gmail.com>
2020-01-23 01:04:53 +0800

    Added jobadds pagination and search capabitly

--- src/controllers/job_add_controller.js
+++ src/controllers/job_add_controller.js
@@ -1,7 +1,10 @@
 
 import HttpStatus from 'http-status-codes'
+
 import JobAdd from '../models/JobAdd'
 import { success, error } from '../util/constants'
+
+const maxNumberOfResults = 100 //max number of results to return in one api call
 
 const getJobById = ( req, res ) => {
 
@@ -17,14 +20,45 @@ const getJobById = ( req, res ) => {
 
 const getJobs = ( req, res ) => {
 
-    JobAdd.find( {} ).exec().then( data => {
+    const offset= Math.max(req.query.offset , 0)
+    const limit =  Math.max(maxNumberOfResults, req.query.limit)
+
+    JobAdd.find(formQueryObjec(req.query)).skip(offset).limit(limit).exec().then( data => {
 
         res.status( 200 ).send( success( data ) )
 
-    } ).catch( err => {
+    } ).catch( err =>   {
         console.log( err )
         res.status( HttpStatus.BAD_REQUEST ).send( error() )
     } )
+}
+
+/**
+ * remove offset and limit 
+ * 
+ * create query : AND of like queries
+ * 
+ */
+const formQueryObjec = ( queryObject )=>{ 
+
+    if ( queryObject ) { 
+        delete queryObject.offset
+        delete queryObject.limit
+    }
+
+    //skip skills since it is an array
+    for (var key in queryObject) {
+
+        if ( key == 'skills' ) {
+            queryObject[ key ] = { $elemMatch: { $regex :  '.*' + queryObject[key], $options: "i"}}  
+        }
+        else { 
+            queryObject[key] =  {$regex: ".*"+queryObject[key], $options:"i"}
+        }
+
+    }
+    console.log(queryObject)
+    return queryObject
 }
 
 const postJobs = ( req, res ) => {
