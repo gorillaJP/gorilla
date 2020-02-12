12ebc8e9362b8ff0d6015905bfca9db37b1ef793
dim912 <dim912@gmail.com>
2020-02-13 01:12:06 +0800

    ES query change

---
+++ src/controllers/job_add_search_controller.js
@@ -0,0 +1,32 @@
+import HttpStatus from 'http-status-codes'
+
+import JobAdd from '../models/JobAdd'
+import { success, error } from '../util/constants'
+
+const maxNumberOfResults = 100 //max number of results to return in one api call
+
+/*** search from elastic search*/
+const getJobsPaginated = (req, res) => {
+
+    let offset = req.query.offset ? Math.max(req.query.offset, 0) : 0
+    let limit = req.query.limit ? Math.max(maxNumberOfResults, req.query.limit) : maxNumberOfResults
+
+
+    JobAdd.search({
+        "multi_match": {
+            "query": "java",
+            "fields": ["description"]
+        }
+    }, (err, results) => {
+        if (results) {
+            res.status(200).send(success(results.body.hits.hits.map(u => {
+                u._source._id = u._id
+                return u._source
+            })))
+            return
+        }
+        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err)
+    })
+}
+
+export default { getJobsPaginated }
