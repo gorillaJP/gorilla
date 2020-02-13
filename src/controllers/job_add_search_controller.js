1f3a4c63d6425ba4ea15fdfbce23dacde22123b5
dim912 <dim912@gmail.com>
2020-02-14 02:09:20 +0800

    ES query support limit and offset

--- src/controllers/job_add_search_controller.js
+++ src/controllers/job_add_search_controller.js
@@ -5,31 +5,32 @@ import { success, error } from '../util/constants'
 
 const maxNumberOfResults = 100 //max number of results to return in one api call
 
-/*** search from elastic search*/
+/*** search from elastic search
+*/
 const getJobsPaginated = (req, res) => {
 
-    let offset = req.query.offset ? Math.max(req.query.offset, 0) : 0
-    let limit = req.query.limit ? Math.max(maxNumberOfResults, req.query.limit) : maxNumberOfResults
-
+    let offset = req.query.offset && req.query.offset != '' ? Math.max(req.query.offset, 0) : 0
+    let limit = req.query.limit && req.query.limit != '' ? Math.min(maxNumberOfResults, req.query.limit) : maxNumberOfResults
 
     JobAdd.esSearch({
-        "size": 100,
+        "size": limit,
+        "from": offset,
         "query": {
             "multi_match": {
                 "query": "java",
-                "fields": ["description"]
+                "fields": ["company", "title", "description", "skill"]
             }
         }
     }, (err, results) => {
         if (results) {
-            res.status(200).send(success(formatResposne(results)))
+            res.status(200).send(success(formatResposne(results, limit, offset)))
             return
         }
         res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(err)
     })
 }
 
-const formatResposne = (data) => {
+const formatResposne = (data, limit, offset) => {
 
     var res = data.body.hits.hits.map(u => {
         u._source._id = u._id
@@ -37,8 +38,12 @@ const formatResposne = (data) => {
     })
 
     return {
-        data: res,
-        meta: data.body.hits.total
+        meta: {
+            total: data.body.hits.total.value,
+            limit: limit,
+            offset: offset
+        },
+        data: res
     }
 }
 
