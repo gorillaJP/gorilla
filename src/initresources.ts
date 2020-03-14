9e9ca24efcb426f6db5c3348e5a1c60dc1d51954
dim912 <dim912@gmail.com>
2020-03-15 03:49:07 +0800

    ts integration

---
+++ src/initresources.ts
@@ -0,0 +1,11 @@
+import mongoConnection from "./util/mongoClient";
+import logger from "./util/logger";
+
+//connecting to mongo db
+mongoConnection
+  .then(con => {
+    logger.info("Mongo Connection established");
+  })
+  .catch(e => {
+    logger.error("Error at connecting to mongo");
+  });
