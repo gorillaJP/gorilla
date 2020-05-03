78a89c121cb1fad286481c7fabff1de2ecf7f75b
dim912 <dim912@gmail.com>
2020-05-04 01:19:46 +0800

    test change

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-//https://www.dynu.com/  . DNSs are configured here
+//https://www.dynu.com/  .. DNSs are configured here
 
 const mongo = {
   url: "mongodb://appadmin:appadmingorilla@206.189.133.87:27017/gorilla",
