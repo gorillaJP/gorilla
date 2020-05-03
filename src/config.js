12080b4a5bfcd343bfa85d51a00dea3c1a7f49d9
dim912 <dim912@gmail.com>
2020-05-03 17:06:57 +0800

    google auth2

--- src/config.js
+++ src/config.js
@@ -7,18 +7,18 @@ const mailerConf = {
   port: 587,
   secure: false, // true for 465, false for other ports
   user: "gorillajobslk@gmail.com", // generated ethereal user
-  pass: "1234!@#$", // generated ethereal password
+  pass: "7890&*()", // generated ethereal password
   from: "gorillajobslk@gmail.com", // generated ethereal password
 };
 
 const appDev = {
   rootUrl: "http://localhost:8080/",
-  verifyEmailRedirect: "http://localhost:3000/",
+  verifyEmailRedirect: "http://localhost:3000?login=initial",
 };
 
 const appProd = {
   rootUrl: "http://159.89.161.233:443/",
-  verifyEmailRedirect: "http://159.89.161.233:3000/",
+  verifyEmailRedirect: "http://159.89.161.233:3000?login=initial",
 };
 
 let app;
